a5e1292cfe633683eb4ffeb74f49366355e4a22f
Rodrigo Sandoval <rodrigosandoval@Alveyworld-07.local>
2016-01-06 09:02:30 -0700

    Added files

---
+++ Python.py
@@ -0,0 +1,41 @@
+# Python Exercise		
+
+# Functions
+
+def echo(k):
+	return k
+	
+	
+	
+	
+def swap(frog1, frog2):
+	return frog2, frog1
+	
+# Arithmetic Function
+def reverse(b):
+	return -b
+	
+	
+def main_arithmetic():
+	pass
+	
+def plus(ayy, lmao):
+	return ayy+lmao
+	
+	
+	
+	
+	
+def main():
+	print  reverse(123)
+	print "echo(frog):", echo("frog")
+	print plus(5, 6)
+	print "swap('k',  'mate'):", swap("k", "mate")
+
+main()
+
+
+
+
+
+
---
+++ band-name-generator.py
@@ -0,0 +1,39 @@
+# Band name generator
+import random
+
+
+titles = ["gigantic", "sour", "steamy", "gross", "stupid", 
+	      "ironic", "greasy", "tangy", "smelly", "small",
+	      "inventive", "spherical", "spiritual", "green",
+	      "blue", "pot bellied", "pickled", "prickly"]
+
+adjs =	["tiny", "fat", "shiny", "ecclectic", "fluffy", "bright",
+	     "corrupt", "aggresive", "alarming", "amazing", "magical",
+	     "courageous", "fierce", "colorless", "red", "thoughtless",
+	     "smart", "delirious", "fabulous", "fergalicious", "dangerous"]
+
+plural_nouns = ["apples", "oranges", "kiwis", "clementines",
+				"wildabeasts", "mammoths", "rabbits", "sloths", "crashes",
+				"spices", "eggs", "herbs", "pony tails", "bears", "snitches",
+				"unicorns", "kermits", "signs", "indians", "cowboys", "cheerleaders"]
+
+def title():
+	''' This function chooses a random title for the name'''
+	return random.choice(titles)
+
+def adj():
+	''' This funtion chooses a random tittle for the name'''
+	return random.choice(adjs)
+
+def plural_noun():
+		return random.choice(plural_nouns)
+
+def main():
+	while True:
+			name = raw_input("Enter your name")
+			if name =="q":
+				break
+			random.seed(name)
+			print title(), name, "and the", adj(), plural_noun()
+
+main()
